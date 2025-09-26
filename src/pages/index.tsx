ea4d7296f98a91893f50184673543785797bdf63
Luan Henrique Neumann <luan.neumann.dev@gmail.com>
2025-09-26 14:37:50 -0300

    feat: header component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button";
+import { Header } from "@/components/header";
 import { Geist, Geist_Mono } from "next/font/google";
 
 const geistSans = Geist({
@@ -16,7 +16,7 @@ export default function Home() {
     <div
       className={`${geistSans.className} ${geistMono.className} font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20`}
     >
-      <h2>Site blog</h2>
+      <Header />
     </div>
   );
 }
